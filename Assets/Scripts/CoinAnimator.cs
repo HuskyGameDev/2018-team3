10a23739b7c0ee1aa1457e8b3c7669199d3885b4
Michael Spoehr <spoehrmichael@gmail.com>
2018-10-11 15:49:10 -0400

    Made the coin a prefab and added the animation

---
+++ Assets/Scripts/CoinAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAnimator : MonoBehaviour {
+
+    private float startingY;
+    private int sign = 1;
+    float RotationSpeed = 120f;
+    float TranslationSpeed = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        startingY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * RotationSpeed);
+        transform.Translate(0, Time.deltaTime * sign * TranslationSpeed, 0);
+
+        if (Math.Abs(transform.position.y - startingY) > 0.2)
+        {
+            sign = -sign;
+        }
+    }
+}
