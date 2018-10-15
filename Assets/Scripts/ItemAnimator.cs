e59b2bd5451c4aaee809414a9af1ce827a1eae5a
Evan Overweg <32006764+Kavatika@users.noreply.github.com>
2018-10-15 00:01:55 -0400

    Generalized ItemAnimator.cs for most GameObjects
    Can now attach ItemAnimator script to a gameobject and rotate/translate in desired direction over time. Workaround in place for objects moving out of game bounds, needs testing.

--- Assets/Scripts/ItemAnimator.cs
+++ Assets/Scripts/ItemAnimator.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 
 public class ItemAnimator : MonoBehaviour {
 
-    private float startingY;
-    private int sign = 1;
-    float RotationSpeed = 120f;
-    float TranslationSpeed = 0.5f;
-    float TranslationDistance = 0.2f;
+    private float startingX, startingY, startingZ;
+    private int signX = 1, signY = 1, signZ = 1;
+    public float RotationSpeedX, RotationSpeedY, RotationSpeedZ;
+    public float TranslationSpeedX, TranslationSpeedY, TranslationSpeedZ;
+    public float TranslationDistanceX, TranslationDistanceY, TranslationDistanceZ;
 
     // Use this for initialization
     void Start () {
+        startingX = transform.position.x;
         startingY = transform.position.y;
+        startingZ = transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        transform.RotateAround(transform.position, transform.up, Time.deltaTime * RotationSpeed);
-        transform.Translate(0, Time.deltaTime * sign * TranslationSpeed, 0);
-
-        if (Math.Abs(transform.position.y - startingY) > TranslationDistance)
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * RotationSpeedY);
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * RotationSpeedX);
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * RotationSpeedZ);
+        transform.Translate(Time.deltaTime * signX * TranslationSpeedX, 
+            Time.deltaTime * signY * TranslationSpeedY, 
+            Time.deltaTime * signZ * TranslationSpeedZ);
+        if (Math.Abs(transform.position.x - startingX) > TranslationDistanceX)
         {
-            sign = -sign;
+            signY = -signY;
+        }
+
+        if (Math.Abs(transform.position.y - startingY) > TranslationDistanceY || transform.position.y < 0)
+        {
+            signY = -signY;
+        }
+
+        if (Math.Abs(transform.position.z - startingZ) > TranslationDistanceZ)
+        {
+            signY = -signY;
         }
     }
 }
