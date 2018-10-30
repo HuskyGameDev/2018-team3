76affd7f2a24db8fee5d1681eefd242da28882c0
Andrew Stockero <atstocke@mtu.edu>
2018-10-30 19:35:43 -0400

    Added Checkpoint sound. Changed to different grass footsteps. Added dirt footsteps.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID CHECKPOINT = 612075679U;
         static const AkUniqueID COINPICKUP = 2752428610U;
         static const AkUniqueID FOOTSTEP = 1866025847U;
         static const AkUniqueID JUNGLEAMBIENCE = 2397454474U;
