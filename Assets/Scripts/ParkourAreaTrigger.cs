18c9f836710da6753fd39e34d60e5baac1c8bfc4
unknown <rdmulfor@mtu.edu>
2018-12-09 17:03:25 -0500

    add keys to parkour level

---
+++ Assets/Scripts/ParkourAreaTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkourAreaTrigger : MonoBehaviour {
+	
+	private bool gotKey = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void OnTriggerEnter(Collider other)
+    {
+		GameManager gameManager = FindObjectOfType<GameManager>();
+        if (other.tag == "Player" && !gotKey)
+        {
+			gotKey = true;
+            gameManager.AddOneKeyFragment();
+        }
+    }
+}
